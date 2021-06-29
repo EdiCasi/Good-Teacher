2b0cb2cd4828f1d0eff454e0a5395180674225ac
Razvan <daniciucrazvan@yahoo.com>
2021-06-29 17:07:00 +0300

    Query to retrieve Posts by userId

--- app/src/main/java/review_data_access/ReviewDao.java
+++ app/src/main/java/review_data_access/ReviewDao.java
@@ -8,6 +8,8 @@ import androidx.room.Query;
 import java.util.ArrayList;
 import java.util.List;
 
+import user_data_access.UserEntity;
+
 @Dao
 public interface ReviewDao
 {
@@ -17,4 +19,7 @@ public interface ReviewDao
     @Query("SELECT * FROM reviews")
     List<ReviewEntity> getReviews();
 
+
+    @Query("SELECT * FROM reviews WHERE userId =:userId")
+    List<ReviewEntity> getReviewsFromLoggedUser(Integer userId);
 }
